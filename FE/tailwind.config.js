5fd48ebc87a588c9de1874ceb7295d00016fd30d
dungnt <dungnt@gmail.com>
2024-07-15 22:08:32 +0700

    ddd

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,41 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./*index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        primary: "#00badb",
+      colors:{
+        primary:'#00badb'
       },
       backgroundSize: {
         "50%": "50%",
-      },
-      keyframes: {
-        "text-move": {
-          from: {
-            opacity: 0,
-            paddingTop: "250px",
-          },
-          to: {
-            paddingTop: "0px",
-            opacity: 1,
-          },
+    },
+    keyframes: {
+      'text-move': {
+        'from': {
+          opacity: 0,
+          paddingTop: '250px', // Tailwind sử dụng string cho các giá trị không phải số nguyên
         },
-        "page-effect": {
-          from: {
-            opacity: 0,
-            paddingTop: "200px",
-          },
-          to: {
-            paddingTop: "0px",
-            opacity: 1,
-          },
+        'to': {
+          paddingTop: '0px',
+          opacity: 1,
         },
       },
-      animation: {
-        "text-move": "text-move 1.2s", // Định nghĩa tên animation và thời gian
-        "page-effect": "page-effect 1.5s",
-      },
+      'page-effect':{
+        'from':{
+          opacity: 0,
+          paddingTop: '200px',
+        },
+        'to': {
+          paddingTop: '0px',
+          opacity: 1,
+        },
+      }
+    },
+    animation: {
+      'text-move': 'text-move 1.2s', // Định nghĩa tên animation và thời gian
+      'page-effect':'page-effect 1.5s'
+    },
     },
   },
   plugins: [],
-};
+}
+
