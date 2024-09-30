5f422749b1aaf98012db9a0550fb63ff07635dc2
quyetcot <phanquyet411613@gmail.com>
2024-09-30 21:45:12 +0700

    BE Login and Register

--- BACKEND/routes/api.php
+++ BACKEND/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\Admin\ClientController;
 use App\Http\Controllers\Api\V1\Admin\EmployeeController;
+use App\Http\Controllers\Api\V1\Client\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,28 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+Route::middleware('auth:sanctum')->get('/user', 
+function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+  
 
+Route::post('logout', [AuthController::class, 'logout'])  
+->middleware('auth:sanctum');
 
+Route::prefix("v1/")->group(function () {
 
-
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::prefix("v1/")->group(function(){
-    
     Route::resource('employees', EmployeeController::class);
     Route::resource('clients', ClientController::class);
-    
-
-
-   
-
-
 });
