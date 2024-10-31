f1245da0c7e5c4eaa34faf8575972425b3df4d45
congtrang18 <nguyencongtrang2K4@gmail.com>
2024-10-31 21:11:08 +0700

    merge cường

--- BACKEND/app/Models/order.php
+++ BACKEND/app/Models/order.php
@@ -24,8 +24,8 @@ class Order extends Model
         'ship_user_name',
         'ship_user_phonenumber',
         'ship_user_address',
-        'voucher_id',
-        'voucher_discount',
+        "voucher_id",
+        "voucher_discount"
     ];
     protected static function boot()
     {
