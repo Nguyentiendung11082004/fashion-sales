f4c5389304e739d68c5074c6f566eea5e13d7af8
congtrang18 <nguyencongtrang2K4@gmail.com>
2024-11-19 05:50:29 +0700

    migration hoàn hàng

---
+++ BACKEND/database/migrations/2024_11_19_052331_create_return_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\OrderDetail;
+use App\Models\ReturnRequest;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('return_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(ReturnRequest::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(OrderDetail::class)->constrained()->cascadeOnDelete();
+            $table->integer('quantity');
+            $table->text('image')->nullable();
+            $table->enum('status',['pending', 'approved', 'rejected'])->default('pending');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('return_items');
+    }
+};
