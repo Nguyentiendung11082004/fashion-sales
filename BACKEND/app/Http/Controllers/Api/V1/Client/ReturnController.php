527c989b109c5ced7398c4d00f9f11e032fb36dd
congtrang18 <nguyencongtrang2K4@gmail.com>
2024-11-19 21:14:35 +0700

    migrate return

---
+++ BACKEND/app/Http/Controllers/Api/V1/Client/ReturnController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReturnController extends Controller
+{
+    //
+    
+}
