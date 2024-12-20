c87f9e8b94fb17385a69dd3ef1ac5c5033ea8833
NguyenCuong-08 <manhcuongnguyen352@gmail.com>
2024-12-20 22:31:45 +0700

    update voucher checkout

--- BACKEND/app/Http/Requests/Voucher/StoreVoucherRequest.php
+++ BACKEND/app/Http/Requests/Voucher/StoreVoucherRequest.php
@@ -28,39 +28,21 @@ class StoreVoucherRequest extends FormRequest
             'code' => 'string|max:50|unique:vouchers,code',
             'start_date' => 'required|date|after_or_equal:today|before_or_equal:end_date',
             'end_date' => 'required|date|after_or_equal:start_date',
-            // 'min_order_value' => 'required|numeric|min:0',
+
             'usage_limit' => 'required|integer|min:1',
             'is_active' => 'boolean',
-            // 'discount_type' => 'required|in:fixed,percent',
-            // 'discount_value' => [
-            //     'required_if:discount_type,fixed,percent',  // bắt buộc nếu là fixed hoặc percentage
-            //     'numeric',  // phải là số
-            //     'min:0',  // không được nhỏ hơn 0
-            //     function ($attribute, $value, $fail) {
-            //         if ($this->discount_type == 'percent' && $value > 100) {
-            //             $fail('The ' . $attribute . ' must not be greater than 100 when discount type is percentage.');
-            //         }
-            //     }
-            // ],
             'discount_type' => 'required|in:fixed,percent',
             'discount_value' => [
-                'required_if:discount_type,fixed,percent', // Bắt buộc nếu là fixed hoặc percent
-                'numeric', // Phải là số
-                'min:0', // Không được nhỏ hơn 0
+                'required_if:discount_type,fixed,percent',  // bắt buộc nếu là fixed hoặc percentage
+                'numeric',  // phải là số
+                'min:0',  // không được nhỏ hơn 0
                 function ($attribute, $value, $fail) {
                     if ($this->discount_type == 'percent' && $value > 100) {
                         $fail('The ' . $attribute . ' must not be greater than 100 when discount type is percentage.');
                     }
                 }
             ],
-            'min_order_value' => [
-                'required|numeric|min:0',
-                function ($attribute, $value, $fail) {
-                    if ($this->discount_type == 'fixed' && $value < $this->discount_value) {
-                        $fail('The ' . $attribute . ' must be greater than or equal to discount value when discount type is fixed.');
-                    }
-                }
-            ],
+            'min_order_value' => 'required|numeric|min:0',
             'meta' => 'array', // Kiểm tra xem meta có phải là mảng không
             'meta.*.meta_key' => 'required|string|max:255', // Đảm bảo meta_key không null và là string
             'meta.*.meta_value' => [
