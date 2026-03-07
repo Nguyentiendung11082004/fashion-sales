28f7c2fade726a1fb040f21cfe8a31fe19a0c87b
congtrang18 <nguyencongtrang2k4@gmail.com>
2026-03-07 21:21:44 +0700

    fixbug 419

--- BACKEND/routes/web.php
+++ BACKEND/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-session', function () {
+    return config('session.same_site');
+});
